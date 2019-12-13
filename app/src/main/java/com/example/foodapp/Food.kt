04f3f82ba98defa75e50489bdb35c44d5e64d956
Laoumir Mustapha Abdelkader <21nounou21@gmail.com>
2019-12-13 21:03:48 +0100

    added attributes and constructor to food.kt

--- app/src/main/java/com/example/foodapp/Food.kt
+++ app/src/main/java/com/example/foodapp/Food.kt
@@ -1,2 +1,12 @@
 package com.example.foodapp
 
+class Food {
+    var name:String?=null
+    var des:String?=null
+    var image:Int?=null
+    constructor(name:String, image:Int, Des:String){
+        this.name = name
+        this.image = image
+        this.des = des
+    }
+}
