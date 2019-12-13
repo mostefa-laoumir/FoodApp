1fd97658879c3dee3c3eb5ef64f68729a250424c
Laoumir Mustapha Abdelkader <21nounou21@gmail.com>
2019-12-13 21:49:41 +0100

    added BaseAdapter to adapt data on grid view

--- app/src/main/java/com/example/foodapp/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.foodapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.food_ticket.view.*
 
 class MainActivity : AppCompatActivity() {
+    var adapter:FoodAdapter? = null
     var foodList = ArrayList<Food>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +23,39 @@ class MainActivity : AppCompatActivity() {
         foodList.add(Food("Honey",R.drawable.honey,"It's basically bee's SHIT, you're welcome"))
         foodList.add(Food("Strawberry Ice Cream",R.drawable.strawberry_ice_cream,"Its not even real srawberries but we never tell you!"))
         foodList.add(Food("Sugar Cubes",R.drawable.sugar_cubes,"Just a shaped sugar so we can charge you more!"))
+        adapter = FoodAdapter(this,foodList)
     }
+    class FoodAdapter: BaseAdapter{
+        var food = ArrayList<Food>()
+        var context: Context?=null
+
+
+        constructor(context: Context,food:ArrayList<Food>):super(){
+            this.food=food
+            this.context=context
+        }
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            var foodinst = this.food[position]
+            var inflater = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            var foodView = inflater.inflate(R.layout.food_ticket,null)
+            foodView.ivFood.setImageResource(foodinst.image!!)
+            foodView.tvName.text = foodinst.name
+
+        return foodView
+        }
+
+        override fun getItem(position: Int): Any {
+            return food[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return food.size
+        }
+
+    }
+
 }
